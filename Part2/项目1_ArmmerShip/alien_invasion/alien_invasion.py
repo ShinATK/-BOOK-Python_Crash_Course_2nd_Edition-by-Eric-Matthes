e3a4136970c26479ba7de3c62a6c395c9f00ae15
Shin <37830914+AKMEttie@users.noreply.github.com>
2021-04-07 18:36:33 +0800

    Update Part2

--- Part2/项目1_ArmmerShip/alien_invasion/alien_invasion.py
+++ Part2/项目1_ArmmerShip/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类。"""
@@ -26,6 +27,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._creat_fleet()
 
     def run_game(self):
         """开始游戏的主循环。"""
@@ -84,6 +87,35 @@ class AlienInvasion:
             if bullet.rect.bottom < 0:
                 self.bullets.remove(bullet)
 
+    def _creat_fleet(self):
+        """创建外星人群"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x // (2*alien_width)
+
+        # 计算每行可容纳多少
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height)-ship_height)
+
+        number_rows = available_space_y // (2*alien_height)
+
+        # 创建第一行外星人
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                # 创建一个外星人并将其加入当前行
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """创建一个外星人并放在当前行"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2*alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2*alien.rect.height * row_number
+        self.aliens.add(alien)
+
+
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         # 每次循环都重绘制屏幕
@@ -91,6 +123,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
