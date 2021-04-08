3c94d03904c385f8def75cae7cd1a7206b8d2c1e
Shin <iakameiloveyou@gmail.com>
2021-04-08 17:04:55 +0800

    Update 15-4

--- Part2/项目2_数据可视化/15_生成数据/15_3_随机漫步/15-3.py
+++ Part2/项目2_数据可视化/15_生成数据/15_3_随机漫步/15-3.py
@@ -1,1 +1,26 @@
 # 15-3：分子运动 修改rw_visual.py文件，将其中的ax.scatter()替换为ax.plot()。为模拟花粉在水滴表面的运动路径，向plt.plot()传递rw.x_values和rw.y_values，并指定实参linewidth。请使用5000个点而不是50 000个点。
+
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+    # 创建一个RandomWalk实例
+    rw = RandomWalk()
+    rw.fill_walk()
+    # 将所有的点都绘制出来
+    plt.style.use('classic')
+    # 调整尺寸以适应屏幕(英寸)
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+    point_numbers = range(rw.num_points)
+    # edgecolors='none'是为了去掉每个点的轮廓
+    ax.plot(rw.x_values, rw.y_values, linewidth=5)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input('Make another walk?(y/n):')
+    if keep_running == 'n':
+        break
---
+++ Part2/项目2_数据可视化/15_生成数据/15_3_随机漫步/15-4.py
@@ -0,0 +1,66 @@
+# 15-4：改进的随机漫步 在类RandomWalk中，x_step和y_step是根据相同的条件生成的：从列表[1, -1]中随机选择方向，并从列表[0, 1, 2, 3,
+# 4]中随机选择距离。请修改这些列表中的值，看看对随机漫步路径有何影响。尝试使用更长的距离选择列表（如：0~8），或者将-1从x或y方向列表中删除。
+
+from random import choice
+import matplotlib.pyplot as plt
+
+class RandomWalk:
+    """一个生成随机漫步数据的类"""
+
+    def __init__(self, num_points=5000):
+        """初始化随机漫步的属性"""
+        self.num_points = num_points
+
+        # 所有随机漫步都始于（0，0）
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """计算随机漫步包含的点"""
+
+        # 不断漫步，直到列表达到指定长度
+        while len(self.x_values) < self.num_points:
+
+            # 决定前进方向以及沿这个方向前进的距离。
+            x_direction = choice([1, -1])
+
+            x_distance = choice([0, 1, 2, 3, 4, 5 ,6, 7, 8, 9])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # 拒绝原地散步
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # 计算下一个点的x与y
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+if __name__ == '__main__':
+    while True:
+        # 创建一个RandomWalk实例
+        rw = RandomWalk()
+        rw.fill_walk()
+        # 将所有的点都绘制出来
+        plt.style.use('classic')
+        # 调整尺寸以适应屏幕(英寸)
+        fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+        point_numbers = range(rw.num_points)
+        # edgecolors='none'是为了去掉每个点的轮廓
+        ax.plot(rw.x_values, rw.y_values, linewidth=5)
+
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        plt.show()
+
+        keep_running = input('Make another walk?(y/n):')
+        if keep_running == 'n':
+            break
