583be77e06f41fa2d51e59c4731ca371f653e9b3
Shin <37830914+AKMEttie@users.noreply.github.com>
2021-04-09 13:08:13 +0800

    Update 15-3.py

--- Part2/项目2_数据可视化/15_生成数据/15_3_随机漫步/15-3.py
+++ Part2/项目2_数据可视化/15_生成数据/15_3_随机漫步/15-3.py
@@ -1,4 +1,7 @@
-# 15-3：分子运动 修改rw_visual.py文件，将其中的ax.scatter()替换为ax.plot()。为模拟花粉在水滴表面的运动路径，向plt.plot()传递rw.x_values和rw.y_values，并指定实参linewidth。请使用5000个点而不是50 000个点。
+# 15-3：分子运动 修改rw_visual.py文件，将其中的ax.scatter()替换为ax.plot()。
+# 为模拟花粉在水滴表面的运动路径，
+# 向plt.plot()传递rw.x_values和rw.y_values，并指定实参linewidth。
+# 请使用5000个点而不是50 000个点。
 
 import matplotlib.pyplot as plt
 
